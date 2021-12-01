fcbc9eef936b34f236b6f30e6ff8927608c71994
khaz7810 <khazi.afzalpasha@rackspace.com>
2021-12-01 17:25:35 +0530

    upload to s3

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {
     type    = string
-    default = "AKIAR6L54YVJJFGS57GD"
+    default = "KEY"
 }
 variable "aws_secret_key" {
     type    = string
-    default = "0lVKsWytdCgrhIhF7SifH5qTvm+AFWL1UWAwGYjq"
+    default = "SECRET"
 }
 variable "region" {
     type    = string
