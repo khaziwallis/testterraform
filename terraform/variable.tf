8680baaa273ce8dcf68f4a942a7b1401d69ab5b2
khaz7810 <khazi.afzalpasha@rackspace.com>
2021-12-01 16:56:31 +0530

    updated variable

---
+++ terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+    type    = string
+    default = "<KEY>"
+}
+variable "aws_secret_key" {
+    type    = string
+    default = "<SECRET>"
+}
+variable "region" {
+    type    = string
+    default = "ap-south-1"
+}
